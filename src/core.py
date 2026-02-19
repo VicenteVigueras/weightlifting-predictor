4addbe9af7a8a811c2606c28501d6f6973c139f0
VicenteVigueras <vicente.viguerassilva@gmail.com>
2026-02-18 22:35:51 -0500

    chore: change debug print statements scope in core functionality

--- src/core.py
+++ src/core.py
@@ -13,22 +13,8 @@ df['Experience'] = label_encoder_experience.fit_transform(df['Experience'])
 df['Goal'] = label_encoder_goal.fit_transform(df['Goal'])
 df['Exercise'] = label_encoder_exercise.fit_transform(df['Exercise'])
 
-print("=" * 50)
-print("\u0332".join("\n1. Label Encoding:\n"))
-print("\u0332".join("1a. Before Encoding:\n"))
-print(previous_df.head())
-print("\n")
-print("\u0332".join("1b. After Encoding:\n"))
-print(df[['Exercise']].head())
-print("=" * 50 + "\n")
-
-print("\u0332".join("2. Define Variables:\n"))
 X = df.drop(columns=['Progress'])
-print("Independent variables: \n - ", X.columns.tolist(), "\n")
-
 y = df['Progress']
-print("Dependent variable:\n -", y.name, "\n")
-print("=" * 50)
 
 scaler = StandardScaler()
 X[['Age', 'Weight', 'Height', 'WeightLifted', 'Reps', 'Sets']] \
@@ -57,15 +43,38 @@ def predict_progress(age, weight, height, experience, goal, exercise, weight_lif
     input_data['Goal'] = label_encoder_goal.transform(input_data['Goal'])
     input_data['Exercise'] = label_encoder_exercise.transform(input_data['Exercise'])
 
-    scaled_features = scaler.transform(input_data[['Age', 'Weight', 'Height', 'WeightLifted', 'Reps', 'Sets']])
-    input_data[['Age', 'Weight', 'Height', 'WeightLifted', 'Reps', 'Sets']] = scaled_features
-    input_data = np.c_[np.ones(input_data.shape[0]), input_data[['Age', 'Weight', 'Height', 'Experience', 'Goal', 'Exercise', 'WeightLifted', 'Reps', 'Sets']]]
-    
-    predicted_progress = input_data.dot(beta)
+    scaled_features = scaler.transform(
+        input_data[['Age','Weight','Height','WeightLifted','Reps','Sets']]
+    )
+    input_data[['Age','Weight','Height','WeightLifted','Reps','Sets']] = scaled_features
 
+    input_data = np.c_[np.ones(input_data.shape[0]),
+        input_data[['Age','Weight','Height','Experience','Goal','Exercise','WeightLifted','Reps','Sets']]
+    ]
+
+    predicted_progress = input_data.dot(beta)
     return predicted_progress[0]
 
-progress = predict_progress(23,75,175,"Beginner","Hypertrophy","Bench Press",45,10,4)
-print("\u0332".join("\n3. Predicted Progress:"))
-print(" - Expected: 5 \n - Actual: ", progress)
-print("\n", "=" * 50)
+if __name__ == "__main__":
+
+    print("=" * 50)
+    print("\u0332".join("\n1. Label Encoding:\n"))
+    print("\u0332".join("1a. Before Encoding:\n"))
+    print(previous_df.head())
+    print("\n")
+    print("\u0332".join("1b. After Encoding:\n"))
+    print(df[['Exercise']].head())
+    print("=" * 50 + "\n")
+
+    print("\u0332".join("2. Define Variables:\n"))
+    print("Independent variables: \n - ", X[:,1:].shape, "\n")
+    print("Dependent variable:\n - Progress\n")
+    print("=" * 50)
+
+    progress = predict_progress(
+        23,75,175,"Beginner","Hypertrophy","Bench Press",45,10,4
+    )
+
+    print("\u0332".join("\n3. Predicted Progress:"))
+    print(" - Expected: 5 \n - Actual: ", progress)
+    print("\n", "=" * 50)
