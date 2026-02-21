acd71d4ff54dc68d2456faa9002159ce7ad63d4e
VicenteVigueras <vicente.viguerassilva@gmail.com>
2026-02-21 12:37:23 -0500

    feat: change scope of derive_model function and change iterations in gradient descent calculation to explore if results differ

--- src/derive_model.py
+++ src/derive_model.py
@@ -3,36 +3,42 @@ import numpy as np
 """
 This will be used to derive the model and understand how the beta coefficients are calculated.
 """
+if __name__ == "__main__":
+    print("=" * 50)
+    # Model a sample equation in the form of y = β0​+β1​x (y = mx + b)
+    X = np.array([
+        [1, 1],
+        [1, 2],
+        [1, 3]
+    ])  
 
-# Model a sample equation in the form of y = β0​+β1​x (y = mx + b)
-X = np.array([
-    [1, 1],
-    [1, 2],
-    [1, 3]
-])  
+    y = np.array([3, 5, 7])
 
-y = np.array([3, 5, 7])
-
-# The closer beta gets to [1, 2], the better the model fits the data
-beta_hardcoded = np.array([1.0, 2.0]) # <-- perfect fit
-pred = X @ beta_hardcoded
-print(pred)
+    # The closer beta gets to [1, 2], the better the model fits the data
+    beta_hardcoded = np.array([1.0, 2.0]) # <-- perfect fit
+    pred = X @ beta_hardcoded
+    print(pred)
 
 
-# We model the squared error loss function to measure how wrong a prediciton is
-# This way we look for when loss is minimized 
-def loss(beta):
-    pred = X @ beta
-    return np.sum((pred - y)**2)
+    # We model the squared error loss function to measure how wrong a prediciton is
+    # This way we look for when loss is minimized 
+    def loss(beta):
+        pred = X @ beta
+        return np.sum((pred - y)**2)
 
-# Gradient Descent
-beta = np.zeros(X.shape[1])
+    print("loss of hardcoded beta: ", loss(beta_hardcoded)) # <-- perfect fit, loss should be 0
 
-learning_rate = 0.01
-for i in range(2500):
-    pred = X @ beta
-    grad = 2 * X.T @ (pred - y)
-    beta = beta - learning_rate * grad
+    # Gradient Descent
+    beta = np.zeros(X.shape[1])
+    print("initial beta: ", beta)
 
-print("beta expected: ", beta_hardcoded)
-print("beta actual: ", beta)
+    learning_rate = 0.01
+    for i in range(10):
+        pred = X @ beta
+        grad = 2 * X.T @ (pred - y)
+        beta = beta - learning_rate * grad
+        print(f"iteration {i+1}: beta = {beta}, loss = {loss(beta)}")
+
+    print("beta expected: ", beta_hardcoded)
+    print("beta actual: ", beta)
+    print("=" * 50)
