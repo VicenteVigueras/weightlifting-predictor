54d06a35ace197a42fbf545091e462493b28866c
VicenteVigueras <vicente.viguerassilva@gmail.com>
2026-02-20 21:43:26 -0500

    feat: implement loss function and add gradient descent logic for 2500 iterations

--- src/derive_model.py
+++ src/derive_model.py
@@ -4,7 +4,7 @@ import numpy as np
 This will be used to derive the model and understand how the beta coefficients are calculated.
 """
 
-# model a sample equation in the form of y=β0​+β1​x
+# Model a sample equation in the form of y = β0​+β1​x (y = mx + b)
 X = np.array([
     [1, 1],
     [1, 2],
@@ -13,7 +13,26 @@ X = np.array([
 
 y = np.array([3, 5, 7])
 
-# conclusion - the closer beta gets to [1, 2], the better the model fits the data
-beta = np.array([1.0, 2.0])
-pred = X @ beta
+# The closer beta gets to [1, 2], the better the model fits the data
+beta_hardcoded = np.array([1.0, 2.0]) # <-- perfect fit
+pred = X @ beta_hardcoded
 print(pred)
+
+
+# We model the squared error loss function to measure how wrong a prediciton is
+# This way we look for when loss is minimized 
+def loss(beta):
+    pred = X @ beta
+    return np.sum((pred - y)**2)
+
+# Gradient Descent
+beta = np.zeros(X.shape[1])
+
+learning_rate = 0.01
+for i in range(2500):
+    pred = X @ beta
+    grad = 2 * X.T @ (pred - y)
+    beta = beta - learning_rate * grad
+
+print("beta expected: ", beta_hardcoded)
+print("beta actual: ", beta)
